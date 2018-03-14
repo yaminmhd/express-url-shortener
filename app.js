ed4a194577b030f289d013dab2f88f709d62540e
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-14 12:19:40 +0800

    Added post,get and delete endpoints to API

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require('ejs');
 
+const app = express();
 // load our own helper functions
 const encode = require("./demo/encode");
 const decode = require("./demo/decode");
 
-const app = express();
+//create existingURL array
+const existingURLs = [
+  { id: "1", url: "www.google.com", hash: "MQ==" },
+  { id: "2", url: "www.facebook.com", hash: "Mg==" }
+];
+
+//app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
 // TODO: Implement functionalities specified in README
 app.get("/", function(req, res) {
   res.send("Hello world!");
+});
+
+
+//POST/shorten-url
+app.post('/shorten-url', function(req,res){
+  const url = req.body.url;
+  const hash = encode(url, existingURLs);
+
+  const shortenedUrl = {
+     id: existingURLs.length+1,
+     url,
+     hash
+  }
+
+  existingURLs.push(shortenedUrl);
+  res.send(existingURLs);
+});
+
+
+//GET/expand-url
+app.get("/expand-url/:hash", function(req,res){
+  try {
+    const url = decode(req.params.hash, existingURLs);
+    const obj = existingURLs.filter(item => item["url"] === url);
+    res.send({url})
+  } catch (error) {
+    res.status(404);
+    res.send({
+      message: `There is no long URL registered for hash value '${req.params.hash}'`
+    })
+  }
+});
+
+
+//delete /expand-url:hash
+app.delete('/expand-url/:hash', function(req,res){
+  const item = existingURLs.filter(obj => obj["hash"] === req.params.hash);
+  if(item){
+    res.send(item);
+  }else{
+    res.send({message: "Not found!"})
+  }
 });
 
 // catch 404 and forward to error handler
@@ -28,7 +78,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  console.log(err);
+  res.send("error");
 });
 
 module.exports = app;
